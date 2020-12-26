f51ea7cfa66936e46adcf15ad9847c291010e8cf
Felipe Mamede <felipebrasilmv@hotmail.com>
2020-12-26 20:30:01 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+interface TitleProps {
+  text: string;
+}
+
+function Title(props: TitleProps) {
+  return (
+    <h1>{props.text}</h1>
+  )
+}
+
+function App() {
+  return (
+    <div className="App">
+      <Title text="Titulo 1" />
+      <Title text="Titulo 2" />
+      <Title text="Titulo 3" />
+    </div>
+  );
+}
+
+export default App;
